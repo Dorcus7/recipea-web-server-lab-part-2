8e4687607fd71276c929e7d1b0e36990c61024a7
Dorcus Nagadya <dorcusnagadya889@gmail.com>
2023-12-30 21:14:32 -0800

    completed

--- src/recipe-lab.js
+++ src/recipe-lab.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const fs = require("fs").promises;
 
@@ -10,20 +11,34 @@ app.listen(3000,()=>{
 app.use(express.json());
 
 const getRecipes = async() =>{
-    const recipes = await fs.readFile("../data/recipea-data.json","utf8");
+    const recipes = await fs.readFileSync("../data/recipea-data.json","utf8");
 
 return recipes;
 };
 
 
-const getRecipe = async (id) => {
-    const date = await fs.readFile("../data/recipea-data.json","utf8");
 
 
-    return JSON.parse(data)[id];
-}
+app.post('/new-recipe', (req, res) => {
+    const newRecipe = req.body;
+    const recipes = JSON.parse(fs.readFileSync('./data/recipea-data.json', 'utf8'));
+    recipes.push(newRecipe);
+    fs.writeFileSync('./data/recipea-data.json', JSON.stringify(recipes, null, 2));
+    res.send('Recipe created successfully!')});
+
+    
+app.put('/change-recipe/:id', (req, res) => {
+        const { id } = req.params;
+        const updatedRecipe = req.body;
+        const recipes = JSON.parse(fs.readFileSync('./data/recipea-data.json', 'utf8'));
+        if (id >= 0 && id < recipes.length) {
+          recipes[id] = updatedRecipe;
+          fs.writeFileSync('./data/recipea-data.json', JSON.stringify(recipes, null, 2));
+          res.send('Recipe updated successfully!');
+        } else {
+          res.status(404).send('Recipe not found');
+        }
+      });
 
 
-const deleteRecipe = async (id) =>{
-    const data = await fs.readFile ("../data/recipea-data.json","utf8")
-}
+
